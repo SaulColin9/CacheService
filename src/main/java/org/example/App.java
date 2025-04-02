1d7cf7f294634cf8a2d10c59f035ca743b7b112e
SaulColin9 <Saul.Colin@vizio.com>
2025-04-02 17:27:29 -0600

    Fix

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,9 +9,6 @@ public class App
     public static void main( String[] args )
     {
         try(SimpleCacheService<Integer, String> cache = SimpleCacheService.create()){
-            cache.setRemovalListener(new LoggerRemovalListener<>());
-
         }
-
     }
 }
